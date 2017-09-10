9938c57bbb37e394f22aa52a85e2c9963d9a1ef6
Guilherme Nascimento <brcontainer@yahoo.com.br>
2017-09-10 14:24:03 -0300

    Fixed custom errors

--- system/application/View/debug/error.php
+++ system/application/View/debug/error.php
@@ -7,16 +7,19 @@
 $data = $source['preview'];
 $breakpoint = $source['breakpoint'];
 $lines = count($data);
+$start = $line - $breakpoint;
+$start = $start < 1 ? 0 : $start;
+$breakpoint--;
 ?>
-<pre style="counter-reset: line <?php echo $line - $breakpoint - 1; ?>"><?php
-    for ($i = 0; $i < $lines; $i++) {
-        $data[$i] = trim($data[$i], "\r\n");
+<pre style="counter-reset: line <?php echo $start; ?>"><?php
+for ($i = 0; $i < $lines; $i++) {
+    $data[$i] = trim($data[$i], "\r\n");
 
-        if ($breakpoint === $i) {
-            echo '<span class="hl-line">', htmlspecialchars($data[$i], ENT_QUOTES), '</span>', EOL;
-        } else {
-            echo '<span>', htmlspecialchars($data[$i], ENT_QUOTES), '</span>', EOL;
-        }
+    if ($breakpoint === $i) {
+        echo '<span class="hl-line">', htmlspecialchars($data[$i], ENT_QUOTES), '</span>', EOL;
+    } else {
+        echo '<span>', htmlspecialchars($data[$i], ENT_QUOTES), '</span>', EOL;
     }
+}
 ?></pre>
 </div>

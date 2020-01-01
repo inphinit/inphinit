57cd6f953966521c0344af0851f1f386b269ac19
Guilherme Nascimento <brcontainer@yahoo.com.br>
2020-01-01 02:54:01 -0300

    Improved server command

--- server.bat
+++ server.bat
@@ -3,7 +3,7 @@
 :: Setup PHP and PORT
 set PHP_BIN="C:\php\php.exe"
 set PHP_INI="C:\php\php.ini"
-set HOST_HOST="localhost"
+set HOST_HOST=localhost
 set HOST_PORT=9000
 
 :: Current path
@@ -14,7 +14,7 @@ set CURRENT_PATH=%CURRENT_PATH:~0,-1%
 set ROUTER="%CURRENT_PATH%\system\boot\server.php"
 
 :: Start built in server
-%PHP_BIN% -S %HOST_HOST%:%HOST_PORT% -c %PHP_INI% -t %CURRENT_PATH% %ROUTER%
+%PHP_BIN% -S "%HOST_HOST%:%HOST_PORT%" -c %PHP_INI% -t "%CURRENT_PATH%" %ROUTER%
 
 :: Prevent close if PHP failed to start
 pause
--- server.sh
+++ server.sh
@@ -3,7 +3,7 @@
 # Setup PHP and PORT
 PHP_BIN="/usr/bin/php"
 PHP_INI="/etc/php5/cli/php.ini"
-HOST_HOST="localhost"
+HOST_HOST=localhost
 HOST_PORT=9000
 
 # Used to restore current dir if using command line
@@ -13,4 +13,4 @@ BASEDIR=$(dirname "${0}")
 ROUTER="$BASEDIR/system/boot/server.php"
 
 # Start built in server
-$PHP_BIN -S $HOST_HOST:$HOST_PORT -c $PHP_INI -t $BASEDIR $ROUTER
+$PHP_BIN -S "$HOST_HOST:$HOST_PORT" -c $PHP_INI -t "$BASEDIR" $ROUTER

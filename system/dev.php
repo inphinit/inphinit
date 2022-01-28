6b347a4ce43b8524db903da610752b9cf398bde3
Guilherme Nascimento <brcontainer@yahoo.com.br>
2022-01-28 06:31:47 -0300

    Version 0.5.16 👽
    Improved ./server

--- system/dev.php
+++ system/dev.php
@@ -1,13 +1,13 @@
 <?php
 use Inphinit\Experimental\Debug;
 
-//Inject CSS for debug if necessary
+// Inject CSS for debug if necessary
 Debug::view('before', 'debug.style');
 
-//Display errors
+// Display errors
 Debug::view('error', 'debug.error');
 
-//Display declared classes (uncomment next line for check used classes)
+// Display declared classes (uncomment next line for check used classes)
 #Debug::view('classes', 'debug.classes');
 
 //Display memory usage (uncomment next line for check memory peak usage and time)

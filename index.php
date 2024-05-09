249d5243b0d23f071234c2e022094f89a309b755
Guilherme Nascimento <brcontainer@yahoo.com.br>
2024-05-09 04:15:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php
-// register_shutdown_function(function () {
-//     echo '<br>memory peak usage: ', round(memory_get_peak_usage() / 1024 / 1024, 5), 'MB';
-
-//     $objs = array();
-
-//     foreach (get_declared_classes() as $value) {
-//         $value = ltrim($value, '\\');
-//         $cname = new \ReflectionClass($value);
-
-//         if (false === $cname->isInternal()) {
-//             $objs[$value] = $cname->getDefaultProperties();
-//         }
-//     }
-
-//     echo '<pre>';
-//     print_r($objs);
-//     print_r(get_included_files());
-//     echo '</pre>';
-// });
-
 define('INPHINIT_START', microtime(true));
 define('INPHINIT_ROOT', strtr(__DIR__, '\\', '/') . '/');
 define('INPHINIT_PATH', INPHINIT_ROOT . 'system/');

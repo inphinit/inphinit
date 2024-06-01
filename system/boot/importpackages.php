a2433b40396fdf95b3d4413a3c48d496e265242e
Guilherme Nascimento <brcontainer@yahoo.com.br>
2024-06-01 09:06:17 -0300

    Fixed framework version

--- system/boot/importpackages.php
+++ system/boot/importpackages.php
@@ -3,11 +3,12 @@
  * Usage with command line: php importpackages.php
  */
 
+define('INPHINIT_START', microtime(true));
 define('INPHINIT_ROOT', str_replace('\\', '/', realpath(__DIR__ . '/../..')));
 define('INPHINIT_SYSTEM', INPHINIT_ROOT . '/system');
 define('INPHINIT_COMPOSER', false);
 
-require_once INPHINIT_ROOT . '/vendor/inphinit/framework/src/Inphinit/Packages.php';
+require_once INPHINIT_SYSTEM . '/vendor/inphinit/framework/src/Inphinit/Packages.php';
 
 $packages = new Inphinit\Packages();
 
@@ -15,7 +16,7 @@ $packages->auto();
 
 $logs = $packages->logs();
 
-$packages->save(INPHINIT_ROOT . '/boot/namespaces.php');
+$packages->save(INPHINIT_SYSTEM . '/boot/namespaces.php');
 
 echo 'Importing packages:', PHP_EOL;
 
